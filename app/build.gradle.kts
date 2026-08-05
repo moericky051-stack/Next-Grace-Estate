2a2e581254aa03bda0b272405846db38e483dc07
moericky051-stack <moericky051@gmail.com>
2026-08-05 23:15:00 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,8 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
 
+implementation("com.google.firebase:firebase-storage")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
