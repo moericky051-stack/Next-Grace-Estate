73d2c725c110057522cc4f588208f892220c76e0
moericky051-stack <moericky051@gmail.com>
2026-08-05 20:15:41 +0630

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Project-level build.gradle.kts (Root)
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
