640363e19cfe9861fa55bc92b8176618ad082c78
moericky051-stack <moericky051@gmail.com>
2026-08-05 21:36:34 +0630

    Update PropertyImage.kt

--- app/src/main/java/com/example/ui/components/PropertyImage.kt
+++ app/src/main/java/com/example/ui/components/PropertyImage.kt
@@ -8,8 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import coil.compose.AsyncImage
-import com.example.R
 
 @Composable
 fun PropertyImage(
