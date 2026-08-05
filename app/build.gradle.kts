5de08c9015c23df0dc278365d6d4053f54068f42
moericky051-stack <moericky051@gmail.com>
2026-08-05 20:49:10 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ android {
 
 dependencies {
     // AndroidX Core & Lifecycle
+
+    implementation("androidx.compose.material:material-icons-extended")
+}
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
