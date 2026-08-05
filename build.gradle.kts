b4353b618b1ea3ed87329880e3672404245d7f11
moericky051-stack <moericky051@gmail.com>
2026-08-05 20:26:14 +0630

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-// Project-level build.gradle.kts (Root)
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
 }
