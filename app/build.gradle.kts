8c94dc025aa8f3c4cbce444b61625b35e646f454
moericky051-stack <moericky051@gmail.com>
2026-08-05 23:32:33 +0630

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+
 
     // Room Database
     val roomVersion = "2.6.1"
